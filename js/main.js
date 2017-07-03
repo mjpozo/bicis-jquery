9f5c2ce632a42549344bac08d610582f5cc2c39e
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-02 22:45:26 -0400

    se agrega el archivo jquery-3.1.1.min al index y se arma la primera validacion (nombre)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+
 function validateForm(){
-	/* Escribe tú código aquí */
+	$(document).ready(function(){
+		//acá empezamos a validar
+		//nombre
+		function name(){
+			var idNombre = $("#name").val();
+			if (!(/^[A-Z][a-z]{3,19}$/).test(idNombre)){
+				$(".name-container").append('<span>Nombre inválido. Ingrese nuevamente.');
+			}
+		}
+		name();
+
+		//apellido
+		
+	});
 }
