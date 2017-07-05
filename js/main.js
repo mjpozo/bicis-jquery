a18a544f833df157f6345325edc5f4edd129443c
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-05 16:09:13 -0400

    se prueba con un for y no funciona

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function validateForm(){
 		(function name(){
 			var idNombre = $("#name").val();
 			if (!(/^[A-Z][a-z]{3,19}$/).test(idNombre)){
-				$(".name-container").append('<span>Nombre inválido. Ingrese nuevamente.');
+				$(".name-container").append('<span>Nombre inválido. Ingrese nuevamente.</span>');
 			}
 		})();
 
@@ -14,7 +14,7 @@ function validateForm(){
 		(function lastName(){
 			var idApellido = $("#lastname").val();
 			if (!(/^[A-Z][a-z]{3,19}$/).test(idApellido)){
-				$(".lastname-container").append('<span>Apellido inválido. Ingrese nuevamente.');
+				$(".lastname-container").append('<span>Apellido inválido. Ingrese nuevamente.</span>');
 			}
 		})();
 
@@ -22,7 +22,7 @@ function validateForm(){
 		(function email(){
 			var idCorreo = $("#input-email").val();
 			if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(idCorreo)){
-				$(".email-container").append('<span>Correo inválido. Ingrese nuevamente.');
+				$(".email-container").append('<span>Correo inválido. Ingrese nuevamente.</span>');
 			}
 		})();
 
@@ -30,7 +30,7 @@ function validateForm(){
 		(function contrasena(){
 			var idContrasena = $("#input-password").val();
 			if (idContrasena=="123456" || idContrasena=="098765" || idContrasena=="password" || !(/^(?=.*\d).{6,}$/).test(idContrasena)){
-				$(".form-group").first().append('<span>Contraseña inválida. Ingrese nuevamente.');
+				$(".form-group").first().append('<span>Contraseña inválida. Ingrese nuevamente.</span>');
 			}
 		})();
 
@@ -38,8 +38,19 @@ function validateForm(){
 		//POR ALGUNA RAZÓN ACÁ ME GUARDA EL SPAN VACIO EXTRA EN EL SELECT Y OTRO CON CONTENIDO EN EL DIV CONTENEDOR DE TWITTER (yo creo que next() es el que está interfiriendo)
 		//los tres métodos hacen lo mismo
 		(function selection(){
+			//probamos con esto
 			/*var eleccion = $("select");
-			$.each(eleccion,function(index,element){
+			var large = eleccion.length;
+			for (i=0 ; i<large ; i++){
+				if (eleccion[i]==0){
+					$(".form-group").next().append('<span>Debe escoger una de las opciones.</span>');
+				}
+			}*/
+		//no funciona
+
+
+
+			/*$.each(eleccion,function(index,element){
 				if (element.value == 0){
 					$(".form-group").next().append('<span>Debe escoger una de las opciones.<span>');
 				}
@@ -60,3 +71,6 @@ function validateForm(){
 
 	});
 }
+
+
+
